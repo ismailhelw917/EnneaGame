e5af698fa42d3973c49ae76375ecb7c3a19442e4
ismailhelw917 <happychesspenguin@gmail.com>
2026-03-16 22:16:46 +0200

    ??

---
+++ capacitor.config 5.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.enneagaming.app',
+  appName: 'Enneagaming',
+  webDir: 'dist'
+};
+
+export default config;
